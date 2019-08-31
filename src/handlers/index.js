6747c8780fc766a313403d759b252b6719299305
Muhammad Ali <mr.muhammad.ali@live.com>
2019-08-31 18:32:51 +0500

    updates http-router

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,30 +1,32 @@
 // src
-import { BASE_URL } from '../constants';
+import { BASE_URL } from '../constants'
 import { createInvoice, getInvoiceById } from './invoice'
 import { getPdf } from '../utils/puppeteer'
 import { Values } from '../types'
 
 export const handlers = {
   '/api/invoice': {
-    POST: (values: Values) => createInvoice(values),
-  },
-  '/api/invoice/download': {
-    POST: (values: Values) => {
-      return createInvoice(values).then(res =>
-        getPdf(`${BASE_URL}/${values.invoice.invoiceId}`),
-      )
-    },
-  },
-  '/api/invoice/:id': {
-    GET: async ({ id }) => {
-      return getInvoiceById(id).then(res => {
-        if (!res) {
-          return Promise.reject(new Error("Invoice doesn't exist."))
-        }
+    POST: ({ body: values }) => createInvoice(values),
 
-        const { client, seller, ...invoice } = res
-        return { client, seller, invoice }
-      })
+    '/download': {
+      POST: ({ body: values }) => {
+        return createInvoice(values).then(res =>
+          getPdf(`${BASE_URL}/${values.invoice.invoiceId}`),
+        )
+      },
+    },
+
+    '/:id': {
+      GET: async ({ params: { id } }) => {
+        return getInvoiceById(id).then(res => {
+          if (!res) {
+            return Promise.reject(new Error("Invoice doesn't exist."))
+          }
+
+          const { client, seller, ...invoice } = res
+          return { client, seller, invoice }
+        })
+      },
     },
   },
 }
