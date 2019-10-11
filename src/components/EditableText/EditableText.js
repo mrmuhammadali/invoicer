c9092b093d2138256bd40ee593521e4c733047e6
Muhammad Haseeb <muhammad.haseeb@aeratechnology.com>
2019-10-11 23:20:03 +0500

    UI-fixes: Fix text overflow for long description

--- src/components/EditableText/EditableText.js
+++ src/components/EditableText/EditableText.js
@@ -1,35 +1,37 @@
 // @flow
 // libs
-import React, { useState, useEffect } from 'react'
-import { FastField, FieldProps, useField, useFormikContext } from 'formik'
+import React, { useState, useEffect } from 'react';
+import { FastField, FieldProps, useField, useFormikContext } from 'formik';
 
 // src
-import { useStyles } from './EditableText.styles'
+import { useStyles } from './EditableText.styles';
 
 type Props = FieldProps & {
   className?: string,
-  type?: string,
-}
+  type?: string
+};
 
 export function EditableText(props: Props) {
-  const styles = useStyles(props)
-  const { className, type = 'text', name, placeholder = '' } = props
-  const {
-    values: { isEditable = false, invoice: { items } } = {},
-  } = useFormikContext()
-  const [field] = useField(name)
-  const value = !field.value && type === 'number' ? 0 : field.value
-  const [text, setText] = useState(value)
+  const styles = useStyles(props);
+  const { className, type = 'text', name, placeholder = '' } = props;
+  const { values: { isEditable = false, invoice: { items } } = {} } = useFormikContext();
+  const [field] = useField(name);
+  const value = !field.value && type === 'number' ? 0 : field.value;
+  const [text, setText] = useState(value);
   const onInput = e => {
-    field.onChange(name)(e.currentTarget.innerText)
-  }
+    field.onChange(name)(e.currentTarget.innerText);
+  };
 
   return (
     <div
-      className={[styles.input, className].join(' ')}
+      className={[
+        styles.input,
+        className,
+        field.name.includes('description') && styles.description
+      ].join(' ')}
       contentEditable={isEditable}
       dangerouslySetInnerHTML={{ __html: text }}
       onInput={onInput}
     />
-  )
+  );
 }
--- src/components/EditableText/EditableText.styles.js
+++ src/components/EditableText/EditableText.styles.js
@@ -1,5 +1,5 @@
 // libs
-import makeStyles from '@material-ui/styles/makeStyles'
+import makeStyles from '@material-ui/styles/makeStyles';
 
 export const useStyles = makeStyles({
   input: {
@@ -10,15 +10,17 @@ export const useStyles = makeStyles({
       content: '""',
       borderBottom: 'solid 1px #777',
       transform: 'scaleX(0)',
-      transition: 'transform 250ms ease-in-out',
+      transition: 'transform 250ms ease-in-out'
     },
-
     '&:focus': {
       outline: '0px solid transparent',
 
       '&:after': {
-        transform: 'scaleX(1)',
-      },
-    },
+        transform: 'scaleX(1)'
+      }
+    }
   },
-})
+  description: {
+    maxWidth: '450px;'
+  }
+});
