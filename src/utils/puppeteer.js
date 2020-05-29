9dac5633c92bbabaf710f2a8097eafc188acfd38
Muhammad Ali <mr.muhammad.ali@live.com>
2020-05-29 14:43:38 +0500

    Adds args

--- src/utils/puppeteer.js
+++ src/utils/puppeteer.js
@@ -3,7 +3,7 @@
 import * as puppeteer from 'puppeteer';
 
 export async function getPdf(url: string): Promise<Buffer> {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox']});
   const page = await browser.newPage();
   await page.goto(url, {waitUntil: 'networkidle2'});
   const pdf = await page.pdf({
